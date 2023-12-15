6e077d6b7dba38414a3ee72331fc5a36fd5de214
CaioFernandes07 <caioaires1@gmail.com>
2023-12-15 19:03:25 -0300

    -Feature home finalizada: --desinstalado pacote alpinejs; --Criado layout com navbar; --Criado componente home; --Adicionado icone e imagem da home.

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('livewire.home');
+})->name('home');
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
